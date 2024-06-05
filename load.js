98880e94930723d7c130908bd7238d59b90a7600
atharvotech <148798365+atharvotech@users.noreply.github.com>
2024-06-05 19:36:18 +0530

    update load.js

--- load.js
+++ load.js
@@ -1,10 +1,16 @@
-if (loadImg==NaN||loadImg==undefined){
-    var loadImg = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiIsUaU2g3NU0gK3FXpCAKScOYxXdsmNF3Clmv6OWdN9Pr2JhBJGwXYUqOPFVZZuMs9aVxi70PWPQwN3n0Eh9Xxkdr72W78UyhjC7AGzSUL5-JGhNdTfX7oec73Xmkv-3fp-kvyu882aZ_G500bFLj4JKhqrE2xAd2vkM7TUyq6t4tBKWGx11Ae5V15fKpo/w643-h482/loader.gif";
-}
-document.getElementsByClassName('loader')[0].innerHTML='<img src='+loadImg+'alt="load-img" class="load-img"><h2>Loading...</h2>';
 
-let loader = document.querySelector('.loader');
-window.addEventListener("load", () =>{
-    loader.classList.add('loaded');
-    addEventListener("animationend", () => {document.querySelector('.body').removeChild(loader)});
+var loadImg;
+if (loadImg == NaN) {
+  var loadImg =
+    "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiIsUaU2g3NU0gK3FXpCAKScOYxXdsmNF3Clmv6OWdN9Pr2JhBJGwXYUqOPFVZZuMs9aVxi70PWPQwN3n0Eh9Xxkdr72W78UyhjC7AGzSUL5-JGhNdTfX7oec73Xmkv-3fp-kvyu882aZ_G500bFLj4JKhqrE2xAd2vkM7TUyq6t4tBKWGx11Ae5V15fKpo/w643-h482/loader.gif";
+}
+document.getElementsByClassName("loader")[0].innerHTML =
+  "<img src=" + loadImg + 'alt="load-img" class="load-img"><h2>Loading...</h2>';
+
+let loader = document.querySelector(".loader");
+window.addEventListener("load", () => {
+  loader.classList.add("loaded");
+  addEventListener("animationend", () => {
+    window.body.removeChild(loader);
+  });
 });
