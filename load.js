0e665cd13ce0dcea46cb0823c6e94ff1ecf2a5af
Atharvotech <148798365+atharvotech@users.noreply.github.com>
2025-04-16 17:56:10 +0530

    v1.1

--- load.js
+++ load.js
@@ -1,15 +1,20 @@
 var loadImg;
+let body = document.body
 if (loadImg == undefined) {
-  var loadImg ="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiIsUaU2g3NU0gK3FXpCAKScOYxXdsmNF3Clmv6OWdN9Pr2JhBJGwXYUqOPFVZZuMs9aVxi70PWPQwN3n0Eh9Xxkdr72W78UyhjC7AGzSUL5-JGhNdTfX7oec73Xmkv-3fp-kvyu882aZ_G500bFLj4JKhqrE2xAd2vkM7TUyq6t4tBKWGx11Ae5V15fKpo/w643-h482/loader.gif";
+  var loadImg = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiIsUaU2g3NU0gK3FXpCAKScOYxXdsmNF3Clmv6OWdN9Pr2JhBJGwXYUqOPFVZZuMs9aVxi70PWPQwN3n0Eh9Xxkdr72W78UyhjC7AGzSUL5-JGhNdTfX7oec73Xmkv-3fp-kvyu882aZ_G500bFLj4JKhqrE2xAd2vkM7TUyq6t4tBKWGx11Ae5V15fKpo/w643-h482/loader.gif";
 }
-document.getElementsByClassName("loader")[0].innerHTML =
+let loader = document.createElement('div')
+loader.classList.add('loader')
+loader.innerHTML =
   "<img src=" + loadImg + 'alt="load-img" class="load-img"><h2>Loading...</h2>';
 
-let loader = document.querySelector(".loader");
+  body.insertBefore(loader,body.firstChild)
+
+
 window.addEventListener("load", () => {
   loader.classList.add("loaded");
   loader.addEventListener("animationend", () => {
-    document.body.removeChild(loader);
+    body.removeChild(loader);
   });
 
 });
